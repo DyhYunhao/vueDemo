2f5a584a332927ebfb4a0076d1ca3ef33498e41c
代云浩 <2609159632@qq.com>
2019-01-17 18:51:22 +0800

    由 Cloud Studio 更新，https://studio.dev.tencent.com

--- dayone/index.js
+++ dayone/index.js
@@ -1,4 +1,3 @@
-
 // basic
 var app = new Vue({
   el: "#app",
@@ -39,4 +38,85 @@ Object.freeze(obj);
 var app3 = new Vue({
   el: "#app3",
   data: obj
+});
+
+// 计算属性的getter,setter
+var app4 = new Vue({
+  el: "#app4",
+  data: {
+    firstName: "yh",
+    lastName: "dai"
+  },
+  computed: {
+    fullName: {
+      // getter
+      get: function(){
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter
+      set: function(newValue) {
+        var name = newValue.split(' ')
+        this.firstName = name[0]
+        this.lastName = name[name.length - 1]     
+      }
+    }
+  }
+});
+app4.fullName = "sdfsf aswewew";
+
+// array filter
+var app5 = new Vue({
+  el: "#app5",
+  data: {
+    numbers: [1, 2, 3, 4, 5]
+  },
+  computed: {
+    evenNumbers: function(){
+      return this.numbers.filter(function(numbers){
+        return numbers % 2 === 0
+      })
+    }
+  }
+});
+
+// prop
+Vue.component('blog-post', {
+  props: ['postTitle'],
+  template: '<h3>{{ postTitle }}</h3>'
+});
+
+// 动画
+var app6 = new Vue({
+  el: "#app6",
+  data: {
+    show: true
+  }
+});
+
+// 动画 
+var app7 = new Vue({
+  el: "#app7",
+   data: {
+    show: false
+  },
+  methods: {
+    beforeEnter: function (el) {
+      el.style.opacity = 0
+      el.style.transformOrigin = 'left'
+    },
+    enter: function (el, done) {
+      Velocity(el, { opacity: 1, fontSize: '1.4em' }, { duration: 300 })
+      Velocity(el, { fontSize: '1em' }, { complete: done })
+    },
+    leave: function (el, done) {
+      Velocity(el, { translateX: '15px', rotateZ: '50deg' }, { duration: 600 })
+      Velocity(el, { rotateZ: '100deg' }, { loop: 2 })
+      Velocity(el, {
+        rotateZ: '45deg',
+        translateY: '30px',
+        translateX: '30px',
+        opacity: 0
+      }, { complete: done })
+    }
+  }
 });
